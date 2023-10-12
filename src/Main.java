f8b0906a6c5af839faae9c2dc17c951a3fd6fee5
R3b00tStone <r3b00tstone@gmail.com>
2023-10-12 18:40:53 +0300

    Update

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Triangle first = new Triangle(14.5, 19.3, 24.8);
+        Triangle second = new Triangle(23.2, 16.7, 18.4);
+        System.out.println(first.area());
+        System.out.println(second.area());
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,52 @@
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triangle() {
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+    public double area(){
+        double s = (a + b + c) / 2.0;
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return area;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+}
